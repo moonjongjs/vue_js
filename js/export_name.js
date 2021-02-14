47be98dd022938cec516fc0f36bac0a230f04760
moonjongjs <moonseonjong@naver.com>
2021-02-15 01:28:04 +0900

    Update 2021-02-14 01:27

---
+++ js/export_name.js
@@ -0,0 +1,5 @@
+const a = '안녕하세요';
+const b = (x,y) => x+y;
+console.log(a);
+console.log(b(3,5));
+export { a, b };    
---
+++ js/main.js
@@ -0,0 +1,3 @@
+//import => export_name.js
+import {a,b} from './export_name.js';   
+console.log (`export_name.js 가져온 변수 ${ a } ${ b }`);
