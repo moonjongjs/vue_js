f67f1bec6597e8968fce74c5ba3862a4d4d74aab
moonjongjs <moonseonjong@naver.com>
2021-02-16 17:48:34 +0900

    2021-02-16 17:48

--- js/export_name.js
+++ js/export_name.js
@@ -1,5 +1,5 @@
-const a = '안녕하세요';
-const b = (x,y) => x+y;
-console.log(a);
-console.log(b(3,5));
-export { a, b };    
+let a = '안녕하세요';
+let b = (x,y) => x+y;
+let z = b(3,5);
+console.log(b(10,10));
+export { a, b, z };    
